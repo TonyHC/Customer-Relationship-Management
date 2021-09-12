662ba3de0dbb1412b464572f99c582978a4566d3
Tony Chao <tchao@mail.usf.edu>
2021-09-12 12:25:36 -0400

    Customer DAO Interface

---
+++ web-customer-tracker/src/main/java/com/java/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.java.springdemo.dao;
+
+import java.util.List;
+
+import com.java.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+	public List<Customer> getCustomers();
+}
