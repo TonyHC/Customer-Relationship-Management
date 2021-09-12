6e3ce1722ca6d98b8721c5a6014058827e49f298
Tony Chao <tchao@mail.usf.edu>
2021-09-12 12:30:00 -0400

    Inject CustomerDAO, update the listCustomers() to retrieve data from CustomerDAO and add retrieve data to Spring MVC Model

--- web-customer-tracker/src/main/java/com/java/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/java/springdemo/controller/CustomerController.java
@@ -1,14 +1,33 @@
 package com.java.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.java.springdemo.dao.CustomerDAO;
+import com.java.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	// Need to Inject the Customer DAO
+	// Spring will scan for a Component that implements CustomerDAO interface
+	// If more than one implementation, use the @Qualifier to specify the correct one
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model model) {
+		// Get the List of Customers from Customer DAO
+		List<Customer> customers = customerDAO.getCustomers();
+		
+		// Add Customers to the Spring MVC Model
+		model.addAttribute("customers", customers);
+		
+		// Return the JSP Page Mapping
 		return "list-customers";
 	}
 }
