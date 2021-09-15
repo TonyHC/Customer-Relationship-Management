eca3097dd8b9eae296a0c9be1f188ce600f9bcba
Tony Chao <tchao@mail.usf.edu>
2021-09-14 22:56:35 -0400

    Added Validation Constraints Annotations for Customer's firstName, lastName, and email

--- web-customer-tracker/src/main/java/com/java/springdemo/entity/Customer.java
+++ web-customer-tracker/src/main/java/com/java/springdemo/entity/Customer.java
@@ -12,6 +12,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "customer")
@@ -21,12 +24,21 @@ public class Customer {
 	@Column(name = "id")
 	private int id;
 	
+	@NotNull
+	@Size(min = 1, max = 30)
+	@Pattern(regexp = "[a-zA-Z]+", message = "must contain only characters")
 	@Column(name = "first_name")
 	private String firstName;
 	
+	@NotNull
+	@Size(min = 1, max = 30)
+	@Pattern(regexp = "[a-zA-Z]+", message = "must contain only characters")
 	@Column(name = "last_name")
 	private String lastName;
 	
+	@NotNull
+	@Size(min = 5, max = 50)
+	@Pattern(regexp = "[a-zA-z0-9]+@[a-zA-z0-9]+(.+)[a-zA-Z0-9]+", message = "must follow the format: [x@xxx.xxx]")
 	@Column(name = "email")
 	private String email;
 	
