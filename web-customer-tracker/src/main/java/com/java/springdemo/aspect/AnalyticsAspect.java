9117add1d7cf50382de97ab705adf36c9f478fe6
Tony Chao <tchao@mail.usf.edu>
2021-09-19 14:10:56 -0400

    Aspect Class for Analytics on number of calls to a specific CustomerDAO method

---
+++ web-customer-tracker/src/main/java/com/java/springdemo/aspect/AnalyticsAspect.java
@@ -0,0 +1,35 @@
+package com.java.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.java.springdemo.utils.CustomersMethodCall;
+
+@Aspect
+@Component
+public class AnalyticsAspect {
+	// Logger for AnalyticsAspect Class
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Autowired
+	private CustomersMethodCall customerMethodCalls;
+	
+	@Around("com.java.springdemo.aspect.AspectDeclarations.NumberOfCallsForSaveCustomer()")
+	public void saveCustomerCalls(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		// Display Method we are calling
+		String methodSignature = proceedingJoinPoint.getSignature().toShortString();
+		logger.info("Method: " + methodSignature);
+		
+		// Execute the method
+		proceedingJoinPoint.proceed();
+	
+		// Increase the number of calls to this method by one and log the result
+		customerMethodCalls.numberOfMethodCalls();
+		logger.info("Method Calls: " + String.valueOf(customerMethodCalls.getSaveCustomerCalls()));
+	}
+}
