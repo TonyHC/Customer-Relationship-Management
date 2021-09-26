0e094e1d54da1930429c11eaed65a0bb9c5020b1
Tony Chao <tchao@mail.usf.edu>
2021-09-25 23:23:19 -0400

    Move license related mappings to License Controller

--- web-customer-tracker/src/main/java/com/java/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/java/springdemo/controller/CustomerController.java
@@ -101,26 +101,6 @@ public class CustomerController {
 		return "list-customers";
 	}
 	
-	@GetMapping("/licenses")
-	public String listLicenses(Model model, @RequestParam(required = false) String sort) {
-		List<License> licenses = null;
-		
-		// If sort field provided, use the sort provided and get the List of Licenses from Customer Service
-		if (sort != null) {
-			int sortField = Integer.parseInt(sort);
-			licenses = customerService.getLicenses(sortField);
-		} else {
-			// If no sort field provided, default to sorting by Licenses Expiration Date
-			licenses = customerService.getLicenses(SortUtils.EXPIRATION_DATE);
-		}
-		
-		// Add Licenses to the Spring MVC Model
-		model.addAttribute("licenses", licenses);
-		
-		// Return to list-licenses.jsp
-		return "list-licenses";
-	}
-	
 	@GetMapping("/license")
 	public String listCustomerLicenses(@RequestParam("customerID") int customerID, Model model) {
 		// Get the desired Customer along with its Licenses using the Customer's id from Customer Service
@@ -137,13 +117,5 @@ public class CustomerController {
 		
 		// Return to customer-licenses.jsp
 		return "customer-licenses";
-	}
-	
-	@GetMapping("/deleteLicense")
-	public String deleteLicense(@RequestParam("licenseID") int licenseID) {
-		// Delete the License from Customer Service using License's id
-		customerService.deleteLicense(licenseID);
-		
-		return "redirect:/customer/licenses";
 	}
 }
