8f71106508f05d42b3748280f6146c97d58d728c
Tony Chao <tchao@mail.usf.edu>
2021-09-25 23:27:25 -0400

    Update Spring Config File to support two different data sources with hibernate support

--- web-customer-tracker/src/main/java/com/java/springdemo/config/AppConfig.java
+++ web-customer-tracker/src/main/java/com/java/springdemo/config/AppConfig.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 import org.hibernate.SessionFactory;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +28,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
-@PropertySource("classpath:persistence-mysql.properties")
+@PropertySource({"classpath:persistence-mysql.properties", "classpath:security-persistence-mysql.properties"})
 @ComponentScan(basePackages = "com.java.springdemo")
 public class AppConfig implements WebMvcConfigurer {
 	@Autowired
@@ -46,7 +47,7 @@ public class AppConfig implements WebMvcConfigurer {
 		return viewResolver;
 	}
 	
-	// Define a Bean for Data Source
+	// Define a Bean for Web Customer Tracker Data Source
 	@Bean
 	public DataSource myDataSource() {
 		// Create Connection Source
@@ -71,11 +72,42 @@ public class AppConfig implements WebMvcConfigurer {
 		myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
 		myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
 		myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
 	
 		return myDataSource;
 	}
+	
+	// Define a Bean for Security Data Source
+	@Bean
+	public DataSource securityDataSource() {
+		// Create Connection Source
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		
+		try {
+			securityDataSource.setDriverClass(env.getProperty("security.jdbc.driver"));
+		} catch (PropertyVetoException exception) {
+			throw new RuntimeException(exception);
+		}
+		
+		// For sanity, log url and user just to make sure we are reading the data
+		logger.info("security.jdbc.url = " + env.getProperty("security.jdbc.url"));
+		logger.info("security.jdbc.user = " + env.getProperty("security.jdbc.user"));
+		
+		// Set Database Connection Properties
+		securityDataSource.setJdbcUrl(env.getProperty("security.jdbc.url"));
+		securityDataSource.setUser(env.getProperty("security.jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("security.jdbc.password"));
+		
+		// Set Connection Pool Properties
+		securityDataSource.setInitialPoolSize(getIntProperty("security.connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(getIntProperty("security.connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(getIntProperty("security.connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(getIntProperty("security.connection.pool.maxIdleTime"));
+	
+		return securityDataSource;
+	}
 
-	// Helper Method: Read Environment Property and conver to int
+	// Helper Method: Read Environment Property and convert to int
 	private int getIntProperty(String propertyName) {
 		String propertyValue = env.getProperty(propertyName);
 		int intPropertyValue = Integer.parseInt(propertyValue);
@@ -108,9 +140,45 @@ public class AppConfig implements WebMvcConfigurer {
 		return properties;
 	}
 	
+	// Define the Security SessionFactory
+	@Bean
+	public LocalSessionFactoryBean securitySessionFactory() {
+		// Create Session Factory
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+	
+		// Set the Properties
+		sessionFactory.setDataSource(securityDataSource());
+		sessionFactory.setPackagesToScan(env.getProperty("security.hibernate.packagesToScan"));
+		sessionFactory.setHibernateProperties(getSecurityHibernateProperties());
+		
+		return sessionFactory;
+	
+	}
+
+	// Helper Method: Set Hibernate Properties and return those properties
+	private Properties getSecurityHibernateProperties() {
+		// Set Hibernate Properties
+		Properties properties = new Properties();
+		
+		properties.setProperty("hibernate.dialect", env.getProperty("security.hibernate.dialect"));
+		properties.setProperty("hibernate.show_sql", env.getProperty("security.hibernate.show_sql"));
+		
+		return properties;
+	}
+	
 	@Bean
 	@Autowired
-	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+	public HibernateTransactionManager customerTransactionManager(@Qualifier("sessionFactory") SessionFactory sessionFactory) {
+		// Setup Transaction Manager based on session factory
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		
+		return transactionManager;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager securityTransactionManager(@Qualifier("securitySessionFactory") SessionFactory sessionFactory) {
 		// Setup Transaction Manager based on session factory
 		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
 		transactionManager.setSessionFactory(sessionFactory);
