29888368d4058934bd756a2636235ad06345f54f
Tony Chao <tchao@mail.usf.edu>
2021-09-25 23:25:17 -0400

    Specify the Transactional Manager used

--- web-customer-tracker/src/main/java/com/java/springdemo/service/CustomerServiceImplementation.java
+++ web-customer-tracker/src/main/java/com/java/springdemo/service/CustomerServiceImplementation.java
@@ -2,10 +2,9 @@ package com.java.springdemo.service;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.java.springdemo.dao.CustomerDAO;
 import com.java.springdemo.dao.LicenseDAO;
@@ -23,56 +22,56 @@ public class CustomerServiceImplementation implements CustomerService {
 	private LicenseDAO licenseDAO;
 	
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public List<Customer> getCustomers(int sortField) {
 		// Delegate Call to CustomerDAO
 		return customerDAO.getCustomers(sortField);
 	}
 	
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public void saveCustomer(Customer customer) {
 		// Delegate Call to CustomerDAO
 		customerDAO.saveCustomer(customer);
 	}
 	
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public Customer getCustomer(int customerID) {
 		// Delegate Call to CustomerDAO
 		return customerDAO.getCustomer(customerID);
 	}
 	
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public void deleteCustomer(int customerID) {
 		// Delegate Call to CustomerDAO
 		customerDAO.deleteCustomer(customerID);
 	}	
 	
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public List<Customer> searchCustomers(String searchName) {
 		// Delegte Call to CustomerDAO
 		return customerDAO.searchCustomers(searchName);
 	}	
 	
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public Customer getCustomerLicenses(int customerID) {
 		// Delegate Call to CustomerDAO
 		return customerDAO.getCustomerLicense(customerID);
 	}
 
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public List<License> getLicenses(int sortField) {
 		// Delegate Call to LicenseDAO
 		return licenseDAO.getLicenses(sortField);
 	}
 
 	@Override
-	@Transactional
+	@Transactional("customerTransactionManager")
 	public void deleteLicense(int licenseID) {
 		// Delegate Call to LicenseDAO
 		 licenseDAO.deleteLicense(licenseID);
