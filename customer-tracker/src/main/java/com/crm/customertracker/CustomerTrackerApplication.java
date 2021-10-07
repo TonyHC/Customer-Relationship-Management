6d4b409dfdb034a5d174c87408c922ce96a22e51
Tony Chao <tchao@mail.usf.edu>
2021-10-06 23:21:18 -0400

    Spring Boot Files

---
+++ customer-tracker/src/main/java/com/crm/customertracker/CustomerTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.crm.customertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTrackerApplication.class, args);
+	}
+}
