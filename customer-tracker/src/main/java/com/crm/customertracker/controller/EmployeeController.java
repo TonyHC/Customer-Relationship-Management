f10ea4fa99b6aaf27c006e7d0bcc2fc581485013
Tony Chao <tchao@mail.usf.edu>
2021-10-14 15:18:17 -0400

    Create Controller for Employee mappings

---
+++ customer-tracker/src/main/java/com/crm/customertracker/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.crm.customertracker.controller;
+
+import com.crm.customertracker.entity.security.User;
+import com.crm.customertracker.service.UserService;
+import com.crm.customertracker.utils.PasswordHider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordHider passwordHider;
+
+    @GetMapping("/page")
+    public String getEmployeeHomePage(Model model) {
+        User user = userService.retrieveAuthenticatedPrincipalByUsername();
+
+        model.addAttribute("firstName", user.getFirstName());
+
+        return "employees/employee-homepage";
+    }
+
+    @GetMapping("/profile")
+    public String getEmployeeProfile(Model model) {
+        // Obtain the authenticated User from User Service
+        User user = userService.retrieveAuthenticatedPrincipalByUsername();
+
+        // Add Authenticated User properties to Model Attribute
+        model.addAttribute("firstName", user.getFirstName());
+        model.addAttribute("lastName", user.getLastName());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("employeeId", user.getId());
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("maskedPassword", passwordHider.passwordMasking(user.getPassword()));
+
+        return "employees/employee-profile";
+    }
+}
