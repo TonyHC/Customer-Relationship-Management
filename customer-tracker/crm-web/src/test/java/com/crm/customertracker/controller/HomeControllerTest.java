efbc2bf383b17a18bd1ea2e5752d668ab0989475
Tony Chao <tchao@mail.usf.edu>
2021-11-21 21:03:02 -0500

    Add unit test for home controller

---
+++ customer-tracker/crm-web/src/test/java/com/crm/customertracker/controller/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package com.crm.customertracker.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class HomeControllerTest {
+    @InjectMocks
+    HomeController homeController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+    }
+
+    @DisplayName("Home page")
+    @Test
+    void homePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+}
